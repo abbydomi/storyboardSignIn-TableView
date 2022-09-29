3013ce19cf0d6d39e1292b7e07a6048fd963c4a2
Abby Dominguez <68694729+abbydomi@users.noreply.github.com>
2022-09-29 19:52:43 +0200

    Transition between login and home

---
+++ storyboardSignIn/HomeVC.swift
@@ -0,0 +1,16 @@
+//
+//  HomeVC.swift
+//  storyboardSignIn
+//
+//  Created by Abby Dominguez on 29/9/22.
+//
+
+import UIKit
+class HomeVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
--- storyboardSignIn/ViewController.swift
+++ storyboardSignIn/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var btnLogin: UIButton!
+    @IBOutlet weak var fieldPassword: UITextField!
+    @IBOutlet weak var fieldUser: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func checkUser(btnLogin: UIButton){
+        if 1 == 1{
+            performSegue(withIdentifier: "segueLoginToHome", sender: nil)
+        }
+    }
 
 }
 
