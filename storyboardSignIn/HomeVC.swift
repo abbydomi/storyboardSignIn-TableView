56069ad4759f23f96b76df384e96c1aeddba9c82
Abby Dominguez <68694729+abbydomi@users.noreply.github.com>
2022-09-29 20:55:01 +0200

    TableView con nombres

--- storyboardSignIn/HomeVC.swift
+++ storyboardSignIn/HomeVC.swift
@@ -6,11 +6,30 @@
 //
 
 import UIKit
-class HomeVC: UIViewController {
+class HomeVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let datosTabla = ["David", "Abby", "Diego", "Pedro", "Juan"]
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return datosTabla.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "miCelda", for: indexPath)
+        let name = datosTabla[indexPath.row]
+        cell.textLabel?.text = name
+        return cell
+    }
+    
     
     override func viewDidLoad() {
+        tableView.dataSource = self
+        tableView.delegate = self
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
-
 }
---
+++ storyboardSignIn/TableViewController.swift
@@ -0,0 +1,12 @@
+//
+//  TableViewController.swift
+//  storyboardSignIn
+//
+//  Created by Abby Dominguez on 29/9/22.
+//
+
+import UIKit
+class TableVC: UIViewController {
+    
+    
+}
